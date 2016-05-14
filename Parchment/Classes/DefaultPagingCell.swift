e90b0ce5ec9431e8069a8bee4352df6a0ebdc00c
Martin Rechsteiner <marrechsteiner@gmail.com>
2016-05-15 00:02:56 +0200

    Move DefaultPagingCell into its separate file

---
+++ Parchment/Classes/DefaultPagingCell.swift
@@ -0,0 +1,49 @@
+import UIKit
+
+public class DefaultPagingCell: PagingCell {
+  
+  private var viewModel: PagingCellViewModel?
+  private let titleLabel = UILabel(frame: .zero)
+  
+  public override var selected: Bool {
+    didSet {
+      configureTitleLabel()
+    }
+  }
+  
+  public override init(frame: CGRect) {
+    super.init(frame: frame)
+    configure()
+  }
+  
+  public required init?(coder: NSCoder) {
+    super.init(coder: coder)
+    configure()
+  }
+  
+  public override func setPagingItem(pagingItem: PagingItem, theme: PagingTheme) {
+    if let titleItem = pagingItem as? PagingTitleItem {
+      viewModel = PagingCellViewModel(title: titleItem.title, theme: theme)
+    }
+    configureTitleLabel()
+  }
+  
+  private func configure() {
+    contentView.addSubview(titleLabel)
+    contentView.constrainToEdges(titleLabel)
+  }
+  
+  private func configureTitleLabel() {
+    guard let viewModel = viewModel else { return }
+    titleLabel.text = viewModel.title
+    titleLabel.font = viewModel.font
+    titleLabel.textAlignment = .Center
+    
+    if selected {
+      titleLabel.textColor = viewModel.selectedTextColor
+    } else {
+      titleLabel.textColor = viewModel.textColor
+    }
+  }
+  
+}
