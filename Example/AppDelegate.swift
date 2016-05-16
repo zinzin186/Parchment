8f8fc9f0e9135ff69acdc8b6758d602c607f6233
Martin Rechsteiner <marrechsteiner@gmail.com>
2016-05-16 15:11:41 +0200

    Update default example

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -1,5 +1,8 @@
 import UIKit
 
+// Nothing to see here. Check out ViewController.swift for
+// implementation details.
+
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
   var window: UIWindow?
--- Example/IndexViewController.swift
+++ Example/IndexViewController.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-class ExampleViewController: UIViewController {
+class IndexViewController: UIViewController {
   
   init(index: Int) {
     super.init(nibName: nil, bundle: nil)
@@ -8,12 +8,13 @@ class ExampleViewController: UIViewController {
     
     let label = UILabel(frame: .zero)
     label.font = UIFont.systemFontOfSize(70, weight: UIFontWeightThin)
-    label.textColor = .lightGrayColor()
+    label.textColor = UIColor(red: 95/255, green: 102/255, blue: 108/255, alpha: 1)
     label.text = "\(index)"
     label.sizeToFit()
+    
     view.addSubview(label)
-    view.backgroundColor = .whiteColor()
     view.constrainCentered(label)
+    view.backgroundColor = .whiteColor()
   }
   
   required init?(coder: NSCoder) {
