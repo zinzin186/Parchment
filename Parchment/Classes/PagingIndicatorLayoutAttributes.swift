1b43c995ec2b7e70f746b9fc0c4fd6ac61b1c691
Martin Rechsteiner <marrechsteiner@gmail.com>
2018-08-24 21:10:21 +0200

    Update indicator and border views when changing options

--- Parchment/Classes/PagingIndicatorLayoutAttributes.swift
+++ Parchment/Classes/PagingIndicatorLayoutAttributes.swift
@@ -4,6 +4,23 @@ open class PagingIndicatorLayoutAttributes: UICollectionViewLayoutAttributes {
 
   open var backgroundColor: UIColor?
   
+  override open func copy(with zone: NSZone? = nil) -> Any {
+    let copy = super.copy(with: zone) as! PagingIndicatorLayoutAttributes
+    copy.backgroundColor = backgroundColor
+    return copy
+  }
+  
+  override open func isEqual(_ object: Any?) -> Bool {
+    if let rhs = object as? PagingIndicatorLayoutAttributes {
+      if backgroundColor != rhs.backgroundColor {
+        return false
+      }
+      return super.isEqual(object)
+    } else {
+      return false
+    }
+  }
+  
   func configure(_ options: PagingOptions) {
     if case let .visible(height, index, _, _) = options.indicatorOptions {
       backgroundColor = options.indicatorColor
