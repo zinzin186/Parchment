ca179433e758cb2786d03daed8908a7b8d6cfb84
Martin Rechsteiner <marrechsteiner@gmail.com>
2020-03-25 18:03:56 +0100

    Setup basic UI tests

--- Parchment/Classes/PagingTitleCell.swift
+++ Parchment/Classes/PagingTitleCell.swift
@@ -64,6 +64,7 @@ open class PagingTitleCell: PagingCell {
   }
 
   open func configureAccessibility() {
+    accessibilityIdentifier = viewModel?.title
     contentView.accessibilityLabel = viewModel?.title
     contentView.accessibilityTraits = viewModel?.selected ?? false ? .selected : .none
   }
